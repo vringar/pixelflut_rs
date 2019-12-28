73f1cb9619be169b74d1b48066f93ffe098f2c2e
HeroicKatora <andreas.molzer@gmx.de>
2019-12-28 16:41:10 +0100

    Support image source as command line argument

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 use pixelflut::client::Client;
 use pixelflut::Color;
 use pixelflut::Pixel;
+
+use std::env;
 use std::net::TcpStream;
 
 fn main() -> std::io::Result<()> {
-    let img = image::open("Snooker_triangle.svg.png").unwrap().to_rgb();
+    let image = env::args().nth(1)
+        .unwrap_or_else(|| "Snooker_triangle.svg.png".to_string());
+
+    let image = image::open(&image)
+        .expect("Image not recognized")
+        .to_rgb();
+
     let mut client = Client::new(TcpStream::connect("localhost:1337")?);
-    for (x, y, pixel) in img.enumerate_pixels() {
+    for (x, y, pixel) in image.enumerate_pixels() {
         let image::Rgb([r,g,b]) = *pixel;
         let pixel = Pixel {
             x: x,
@@ -15,9 +23,11 @@ fn main() -> std::io::Result<()> {
         };
         client.write(pixel)?;
     }
+
     let result = client.read(99, 99)?;
     println!("{}", result.to_string());
     let (x, y) = client.size()?;
     println!("SIZE {}, {}", x, y);
+
     Ok(())
 }
