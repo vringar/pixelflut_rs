f31f38e1d92c7fe65b65667f5b4f58ce39846118
Stefan Zabka <stefan.zabka@hu-berlin.de>
2019-12-28 04:29:21 +0100

    First steps with structs and impls

--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,63 @@ use std::io::prelude::*;
 use std::net::TcpStream;
 
 fn main() -> std::io::Result<()> {
-    let mut stream = TcpStream::connect("localhost:1337")?;
-    for i in 0..100 {
-        let pixel = Pixel{x:i, y: i, color: Color(99,99,99, None)};
-        let message = pixel.to_string() + "\n";
-        stream.write(&message.as_bytes())?;
+    let stream = TcpStream::connect("localhost:1337")?;
+    let mut client = PixelFlutClient { stream };
+    for i in 100..200 {
+        let pixel = Pixel {
+            x: i,
+            y: i,
+            color: Color(99, 99, 99, None),
+        };
+        client.write(pixel)?;
     }
-    println!("The message was sent.");
+    let result = client.read(99, 99)?;
+    println!("{}", result.to_string());
     Ok(())
 }
 struct PixelFlutClient {
     stream: TcpStream,
 }
+
 impl PixelFlutClient {
-    fn read(&mut self, x: u32, y: u32) -> std::io::Result<Pixel> {
+    pub fn read(&mut self, x: u32, y: u32) -> std::io::Result<Pixel> {
+        self.stream.write(format!("PX {} {}\n", x, y).as_bytes())?;
+
+        let result = self.read_line()?;
+        let result  = result.split_whitespace()
+            .last()
+            .expect("There was no string to split");
+        let r = result[0..2].parse::<u8>().unwrap();
+        let g = result[2..4].parse::<u8>().unwrap();
+        let b = result[4..].parse::<u8>().unwrap();
+        Ok(Pixel {
+            x,
+            y,
+            color: Color(r, g, b, None),
+        })
+    }
+    pub fn write(&mut self, p: Pixel) -> std::io::Result<()> {
+        let message = p.to_string() + "\n";
+        self.stream.write(&message.as_bytes())?;
+        Ok(())
+    }
+    pub fn size(&mut self) -> std::io::Result<(u32, u32)> {
+        todo!();
+    }
+    fn read_line(&mut self) -> std::io::Result<String> {
         let mut result: Vec<u8> = vec![0; 1];
         while {
             let mut buf = vec![0; 1];
             self.stream.read(&mut buf[..])?;
             result.append(&mut buf);
-            result.last().expect("Idk what I'm doing anyways") != &b'\n'
+            result.last().expect("The buffer was empty") != &b'\n'
         } {}
-        todo!();
-    }
-    fn write(&mut self, p: Pixel) -> std::io::Result<()> {
-        todo!();
-    }
-    fn size(&mut self) -> std::io::Result<(u32, u32)> {
-        todo!();
+        Ok(String::from_utf8(result).expect("The pixelflut returned invalid UTF-8"))
     }
 }
 #[derive(Default)]
 struct Color(u8, u8, u8, Option<u8>);
+
 impl ToString for Color {
     fn to_string(&self) -> String {
         let Color(r, g, b, a) = self;
@@ -44,6 +69,7 @@ impl ToString for Color {
         }
     }
 }
+
 #[derive(Default)]
 struct Pixel {
     x: u32,
